525ca77c671c088b7b7901cc4d7a1f74c5f38047
jaimiho <jaimiho@gmail.com>
2017-07-13 22:52:02 -0500

    first commit

---
+++ Application/src/Application.java
@@ -0,0 +1,26 @@
+
+public class Application {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		
+		int myNumber = 88;
+		short myShort = 847;
+		long myLong = 101;
+		double myDouble = 7.43445;
+		float myFloat = 323f;
+		boolean myBoolean = false;
+		char myChar = 'J';
+		byte myByte = 127;
+		
+		System.out.println(myNumber);
+		System.out.println(myShort);
+		System.out.println(myLong);
+		System.out.println(myDouble);
+		System.out.println(myFloat);
+		System.out.println(myBoolean);
+		System.out.println(myChar);
+		System.out.println(myByte);
+	}
+
+}
